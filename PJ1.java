4b2b403f00f8cde7aa0bf95dd17b329d7545fb4e
user-default <71413474+user-default@users.noreply.github.com>
2020-10-13 21:56:14 -0300

    PJ1.java

--- PJ1.java
+++ PJ1.java
@@ -10,34 +10,34 @@ public class Principal
 {
 	public static void main(String []args) throws IOException
 	{
-		String path = "/home/user/example.txt";
+		String path = "/home/ermac/Códigos/Java/POO_Projeto_1UN/file.txt";
 		Scanner teclado = new Scanner(System.in);
-		int insere = 1, exibe = 2, apaga = 3, i = 0;
+		int i = 0;
 		System.out.println("Manipulador de arquivos de textos iniciado!\n");
 		System.out.println("Opções:\n1 - Para Inserir caracteres no arquivo;\n2 - Exibir todos os nomes contidos no arquivo;\n3 - Apagar todo o arquivo;\n0 - Para encerrar o programa;\n");		
 		while(1 != 0)
 		{
-			System.out.println("Digite uma das opções:\n");
+			System.out.println("\nDigite uma das opções:\n");
 			i  = teclado.nextInt();
 			if(i == 0)
 			{
-				System.out.println("Fim de execução!\n");
+				System.out.println("\nFim de execução!\n");
 				System.exit(0);
 			}	
-			else if(i == insere)
+			else if(i == 1)
 			{
 				ManipuladorDeArquivos.escritor(path);
 			}
-			else if(i == exibe)
+			else if(i == 2)
 			{
 				ManipuladorDeArquivos.leitor(path);
 			}
-			else if(i == apaga)
+			else if(i == 3)
 			{
 				ManipuladorDeArquivos.apagador(path);
 			}
 			else
-				System.out.println("Opção inválida!\n");
+				System.out.println("\nOpção inválida!\n");
 			
 		}
 
@@ -70,7 +70,7 @@ class ManipuladorDeArquivos
 		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
 		String linha = "";
 		Scanner input = new Scanner(System.in);
-		System.out.println("Escreva algo: ");
+		System.out.println("\nEscreva algo:\n");
 		linha = input.nextLine();
 		buffWrite.append(linha + "\n");
 		buffWrite.close();
@@ -84,3 +84,4 @@ class ManipuladorDeArquivos
 	}
 
 }
+
